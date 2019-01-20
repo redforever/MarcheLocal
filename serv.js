d149f4010095a9aa9354e261c7338bca7dfb28dc
red red <redforever258654@gmail.com>
2019-01-20 08:42:28 +0100

    marche local

--- serv.js
+++ serv.js
@@ -2,7 +2,7 @@ var  express = require('express'),
      app = express(),
      server = require('http').createServer(app),
      io = require('socket.io').listen(server);
-   
+     var $ = require('jquery');
 
 var port = process.env.PORT || 8080;
  
